227002270cade8b52b691ebf5c957b96f51bc1ce
divashusaini <dishusaini2july@gmail.com>
2026-04-21 15:43:16 +0530

    Cjs vs Mjs

---
+++ backend/day2/cal.js
@@ -0,0 +1,7 @@
+const {sum,mul,sub}= require("./calculations");
+
+sum(2,4);
+sub(3,4);
+mul(6,8);
+
+console.log("Hello ji");
---
+++ backend/day2/calculations/index.js
@@ -0,0 +1,5 @@
+const mul = require("./mul.js");
+const sum = require("./sum.js");
+const sub = require("./sub.js");
+
+module.exports={mul,sum,sub};
