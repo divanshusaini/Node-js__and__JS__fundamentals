7962f7ce032d263d03a5f0ecf6bb4eff166cd433
divashusaini <dishusaini2july@gmail.com>
2026-05-01 17:03:59 +0530

    routes

--- backend/day4/index.js
+++ backend/day4/index.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const Server = http.createServer((req,res)=>{
+    if(req.url==='/')
        res.end("Hello coders");
+    else if (req.url==="/contact")
+        res.end("this is your contact page ");
+     else if (req.url==="/about")
+        res.end("this is your about page ");
+    else
+        res.end("this is not valid url");
+
 });
 
 Server.listen(4000,()=>{
